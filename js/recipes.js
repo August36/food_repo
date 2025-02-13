d2a243389a9cc5c6cd89cf731bf930a40c433e2f
August <augustjp@live.dk>
2025-02-13 11:04:42 +0100

    template version

--- js/recipes.js
+++ js/recipes.js
@@ -31,33 +31,112 @@ const DEFAULT_RECIPES = 10;
 
 
 //This is the right way to do it - creating the html
-const showRandomRecipesWithInnerHTMLAndOnePageRefresh = async (numRecipes = DEFAULT_RECIPES) => {
+// const showRandomRecipesWithInnerHTMLAndOnePageRefresh = async (numRecipes = DEFAULT_RECIPES) => {
 
-    let recipeList = '';
-    for (let index = 0; index < numRecipes; index++) {
+//     let recipeList = '';
+//     for (let index = 0; index < numRecipes; index++) {
 
-        await fetch(`${BASE_URL}/random.php`)
-        .then(response => response.json())
-        .then(data => {
-            data = data.meals[0];
-            recipeList += `
-                <article>
-                    <header>
-                        <h2>${data.strMeal}</h2>
-                    </header>
-                    <img src="${data.strMealThumb}/preview" alt="${data.strMeal}">
-                    <div>
-                        <p class="pill">${data.strCategory}</p>
-                        <p class="pill">${data.strArea}</p>
-                    </div>
-                </article>
-            `;
-        })
-        .catch(error => console.log(error));
+//         await fetch(`${BASE_URL}/random.php`)
+//         .then(response => response.json())
+//         .then(data => {
+//             data = data.meals[0];
+//             recipeList += `
+//                 <article>
+//                     <header>
+//                         <h2>${data.strMeal}</h2>
+//                     </header>
+//                     <img src="${data.strMealThumb}/preview" alt="${data.strMeal}">
+//                     <div>
+//                         <p class="pill">${data.strCategory}</p>
+//                         <p class="pill">${data.strArea}</p>
+//                     </div>
+//                 </article>
+//             `;
+//         })
+//         .catch(error => console.log(error));
 
-        document.querySelector('#recipe-list').innerHTML = recipeList;
-    }
-};
+//         document.querySelector('#recipe-list').innerHTML = recipeList;
+//     }
+// };
+
+// const showRandomRecipes = async (numRecipes = DEFAULT_RECIPES) => {
+
+//     const fragment = document.createDocumentFragment();
+//     for (let index = 0; index < numRecipes; index++) {
+
+//         await fetch(`${BASE_URL}/random.php`)
+//         .then(response => response.json())
+//         .then(data => {
+//             data = data.meals[0];
+
+//             const h2 = document.createElement('h2');
+//             h2.innerText = data.strMeal;
+
+//             const header = document.createElement('header');
+//             header.append(h2);
+
+//             const img = document.createElement('img');
+//             img.setAttribute('src', `${data.strMealThumb}/preview`);
+//             img.setAttribute('alt', data.strMeal);
+
+//             const pCategory = document.createElement('p');
+//             pCategory.classList.add('pill');
+//             pCategory.innerText = data.strCategory;
+
+//             const pArea = document.createElement('p');
+//             pArea.classList.add('pill');
+//             pArea.innerText = data.strArea;
+            
+//             const div = document.createElement('div');
+//             div.append(pCategory);
+//             div.append(pArea);
+
+//             const article = document.createElement('article');
+//             article.append(header);
+//             article.append(img);
+//             article.append(div);
+
+//             fragment.append(article);
+//         })
+//         .catch(error => console.log(error));
+//     }
+//     document.querySelector('#recipe-list').append(fragment);
+// };
+
+
+
+//templates
+
+// const showRandomRecipesWithInnerHTMLAndOnePageRefresh = async (numRecipes = DEFAULT_RECIPES) => {
+
+//     let recipeList = '';
+//     for (let index = 0; index < numRecipes; index++) {
+
+//         await fetch(`${BASE_URL}/random.php`)
+//         .then(response => response.json())
+//         .then(data => {
+//             data = data.meals[0];
+//             recipeList += `
+//                 <article>
+//                     <header>
+//                         <h2>${data.strMeal}</h2>
+//                     </header>
+//                     <img src="${data.strMealThumb}/preview" alt="${data.strMeal}">
+//                     <div>
+//                         <p class="pill">${data.strCategory}</p>
+//                         <p class="pill">${data.strArea}</p>
+//                     </div>
+//                 </article>
+//             `;
+//         })
+//         .catch(error => console.log(error));
+
+//         document.querySelector('#recipe-list').innerHTML = recipeList;
+//     }
+// };
+
+
+//How to do it with templates
 
 const showRandomRecipes = async (numRecipes = DEFAULT_RECIPES) => {
 
@@ -68,38 +147,22 @@ const showRandomRecipes = async (numRecipes = DEFAULT_RECIPES) => {
         .then(response => response.json())
         .then(data => {
             data = data.meals[0];
-
-            const h2 = document.createElement('h2');
-            h2.innerText = data.strMeal;
-
-            const header = document.createElement('header');
-            header.append(h2);
-
-            const img = document.createElement('img');
-            img.setAttribute('src', `${data.strMealThumb}/preview`);
-            img.setAttribute('alt', data.strMeal);
-
-            const pCategory = document.createElement('p');
-            pCategory.classList.add('pill');
-            pCategory.innerText = data.strCategory;
-
-            const pArea = document.createElement('p');
-            pArea.classList.add('pill');
-            pArea.innerText = data.strArea;
             
-            const div = document.createElement('div');
-            div.append(pCategory);
-            div.append(pArea);
+            const card = document.querySelector('#recipe-card').content.cloneNode(true);
 
-            const article = document.createElement('article');
-            article.append(header);
-            article.append(img);
-            article.append(div);
+            card.querySelector("h2").innerText = data.strMeal
 
-            fragment.append(article);
+            const img = card.querySelector("img");
+            img.setAttribute('src', `${data.strMealThumb}/preview`);
+            img.setAttribute('alt', data.strMeal)
+
+            card.querySelector('.pill:first-of-type').innerText = data.strCategory;
+            card.querySelector(".pill:last-of-type").innerText = data.strArea;
+
+            fragment.append(card);
         })
         .catch(error => console.log(error));
-    }
+        }
     document.querySelector('#recipe-list').append(fragment);
 };
 
